335568c79f3275600a05443a823f0869938db89e
JavaThorman <120687540+JavaThorman@users.noreply.github.com>
2023-02-17 15:44:30 +0100

    changes

--- src/Paraply.java
+++ src/Paraply.java
@@ -4,66 +4,53 @@ public class Paraply {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        final boolean alwaysTrue = true;
-        final boolean alwaysFalse = false;
+
         String regnString;
         boolean regn = false;
-        boolean regn1tim = false; // initialize to false
+        boolean regn1tim = false;
         String regn1timString;
-        System.out.println("Is it raining now? (True/False - Y/N)");
+
+        System.out.println("Regnar det nu? (True/False - Y/N)");
         regnString = input.next();
 
-        System.out.println("Kommer det att regna inom 1 timme?");
+        System.out.println("Kommer det att regna inom närmsta timmen? (True/False - Y/N)");
         regn1timString = input.next();
 
-              //om det regnar nu
-         if (regnString.equalsIgnoreCase("y") || regnString.equalsIgnoreCase("yes") || regnString.equalsIgnoreCase("true")
-               || regnString.equalsIgnoreCase("sant") || regnString.equalsIgnoreCase("ja")) {
-             regn = true;
+        // Kolla om det regnar
+        if (regnString.equalsIgnoreCase("y") || regnString.equalsIgnoreCase("yes") || regnString.equalsIgnoreCase("true")
+                || regnString.equalsIgnoreCase("sant") || regnString.equalsIgnoreCase("ja")) {
+            regn = true;
 
-                //om det inte regnar nu
-           } else if (regnString.equalsIgnoreCase("n") || regnString.equalsIgnoreCase("no") || regnString.equalsIgnoreCase("false")
-                 || regnString.equalsIgnoreCase("falskt") || regnString.equalsIgnoreCase("nej")) {
-             regn = false;
+            // Kolla om det kommer att regna inom närmsta timme
+            if (regn1timString.equalsIgnoreCase("y") || regn1timString.equalsIgnoreCase("yes") || regn1timString.equalsIgnoreCase("true")
+                    || regn1timString.equalsIgnoreCase("sant") || regn1timString.equalsIgnoreCase("ja")) {
+                regn1tim = true;
 
-                 // om det regnar om en 1 tim
-             if (regn1timString.equalsIgnoreCase("y") || regn1timString.equalsIgnoreCase("yes") || regn1timString.equalsIgnoreCase("true")
-                     || regn1timString.equalsIgnoreCase("sant") || regn1timString.equalsIgnoreCase("ja")) {
-                 regn1tim = true;
+            } else if (regn1timString.equalsIgnoreCase("n") || regn1timString.equalsIgnoreCase("no") || regn1timString.equalsIgnoreCase("false")
+                    || regn1timString.equalsIgnoreCase("falskt") || regn1timString.equalsIgnoreCase("nej")) {
+                regn1tim = false;
+            }
 
-                 // om det inte kommer regna om 1 tim
-             } else if (regn1timString.equalsIgnoreCase("n") || regn1timString.equalsIgnoreCase("no") || regn1timString.equalsIgnoreCase("false")
-                     || regn1timString.equalsIgnoreCase("falskt") || regn1timString.equalsIgnoreCase("nej")) {
-                 regn1tim = false;
+            if (regn && regn1tim) {
+                System.out.println("Du behöver ett paraply.");
+            } else if (regn && !regn1tim) {
+                System.out.println("Kanske borde du ta med ett paraply, för säkerhets skull!.");
+            } else {
+                System.out.println("äsch, du behöver inget paraply!");
+            }
 
+        } else if (regnString.equalsIgnoreCase("n") || regnString.equalsIgnoreCase("no") || regnString.equalsIgnoreCase("false")
+                || regnString.equalsIgnoreCase("falskt") || regnString.equalsIgnoreCase("nej")) {
 
-                     // Om det börjar regna, och det kommer regna inom 1 timme
-             }
-             if (regn1timString.equalsIgnoreCase("y") || regn1timString.equalsIgnoreCase("yes") || regn1timString.equalsIgnoreCase("true")
-                     || regn1timString.equalsIgnoreCase("sant") || regn1timString.equalsIgnoreCase("ja") || regnString.equalsIgnoreCase("y") || regnString.equalsIgnoreCase("yes") || regnString.equalsIgnoreCase("true")
-                     || regnString.equalsIgnoreCase("sant") || regnString.equalsIgnoreCase("ja")) {
-                 regn = true;
-                 regn1tim = true;
+            if (regn1timString.equalsIgnoreCase("y") || regn1timString.equalsIgnoreCase("yes") || regn1timString.equalsIgnoreCase("true")
+                    || regn1timString.equalsIgnoreCase("sant") || regn1timString.equalsIgnoreCase("ja")) {
+                System.out.println("Du behöver inget paraply.");
+            } else {
+                System.out.println("Du behöver inget paraply.");
+            }
 
-             }
-             if (regn1timString.equalsIgnoreCase("n") || regn1timString.equalsIgnoreCase("no") || regn1timString.equalsIgnoreCase("false")
-                     || regn1timString.equalsIgnoreCase("falskt") || regn1timString.equalsIgnoreCase("nej") || regnString.equalsIgnoreCase("n") || regnString.equalsIgnoreCase("no") || regnString.equalsIgnoreCase("false")
-                     || regnString.equalsIgnoreCase("falskt") || regnString.equalsIgnoreCase("nej")) {
-                 regn = false;
-                 regn1tim = false;
-             }
-boolean wasWhileExecuted = false;
-             while (regn1tim == alwaysTrue && regn == alwaysTrue) {
-                 System.out.println("Behöver du ett paraply?: " + regn1tim);
-                 wasWhileExecuted = true;
-                 break;
-             }
-
-             while (regn == alwaysTrue && regn1tim == alwaysFalse) {
-                 System.out.println("Kanske att du behöver ett paraply. Ta med ett - just in case!");
-                 break;
-             }
-
-         }
+        } else {
+            System.out.println("Ogiltiga svar!");
+        }
     }
 }
